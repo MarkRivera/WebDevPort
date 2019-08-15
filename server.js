4fba3e4a49635da24960afb4ad31ee82a3345d0a
Mark Rivera <Mrivera1991@gmail.com>
2019-08-14 21:35:35 -0400

    fixed navigation

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 5000;
 console.log(path.join(__dirname, "build"));
 app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "build")));
+app.get("*", function(req, res) {
+	res.sendFile(__dirname + "/build/index.html");
+});
 app.use("/api/resume", require("./routes/resume"));
 app.use("/api/contact", require("./routes/contact"));
+app.use(function(req, res, next) {
+	res.status(404).send("File not found");
+});
 
 app.listen(port, () => console.log(`App listening on ${port}`));
